965ff14a319790bd3d672071038388db83710ab8
Phi Long <shamt2004@gmail.com>
2025-11-09 22:44:16 +0700

    [LongTNP]: layout login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,7 @@ dependencies {
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.squareup.okhttp3:logging-interceptor:4.11.0")
+
+
+    implementation("com.google.android.material:material:1.1.0-alpha02")
 }
